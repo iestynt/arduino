c3743a046976a9b256f7679f128302d3f8e1c112
Iestyn Tronson <iestynt@LONLAP09076MAC.local>
2019-01-02 15:02:27 +0000

    add new year files

--- mearm.ino
+++ mearm.ino
@@ -1,53 +1,73 @@
+//zoomkat dual pot/servo test 12-29-12
+//view output using the serial monitor
 
-int potentiometerPin = 0;
+#include <Servo.h> 
+Servo myservo1;
+Servo myservo2;
+Servo myservo3;
+Servo myservo4;
 
+int potpin1 = 0;  //my pot pin
+int potpin2 = 1;
+int potpin3 = 2;
+int potpin4 = 3;
 
-int ledPin = 11;
+int newval1, oldval1;
+int newval2, oldval2;
+int newval3, oldval3;
+int newval4, oldval4;
 
-int potentiometerVal = 0;
-
-
-
-void setup()
-
+void setup() 
 {
-
-  Serial.begin(9600);          //  setup serial
-
+  Serial.begin(9600);  
+  myservo1.attach(5);  
+  myservo2.attach(9);
+  myservo3.attach(10);
+  myservo4.attach(11);
+  Serial.println("testing dual pot servo");  
 }
-
-
 
 void loop()
+{ 
+  newval1 = analogRead(potpin1);           
+  newval1 = map(newval1, 0, 1023, 0, 179); 
+  if (newval1 < (oldval1-2) || newval1 > (oldval1+2)){  
+    myservo1.write(newval1);
+    Serial.print("1- ");
+    Serial.println(newval1);
+    oldval1=newval1;
+  }
 
-{
+  newval2 = analogRead(potpin2);
+  newval2 = map(newval2, 0, 1023, 0, 179);
+  if (newval2 < (oldval2-2) || newval2 > (oldval2+2)){  
+    myservo2.write(newval2);
+    Serial.print("2- ");    
+    Serial.println(newval2);
+    oldval2=newval2;
+  }
+  delay(50);
+  {
+  }
+  
+  newval3 = analogRead(potpin3);           
+  newval3 = map(newval3, 0, 1023, 0, 179); 
+  if (newval1 < (oldval1-2) || newval3 > (oldval3+2)){  
+    myservo1.write(newval3);
+    Serial.print("3- ");
+    Serial.println(newval3);
+    oldval3=newval3;
+  }
+  {
+  } 
+  newval4 = analogRead(potpin4);           
+  newval4 = map(newval4, 0, 1023, 0, 179); 
+  if (newval1 < (oldval1-2) || newval4 > (oldval4+2)){  
+    myservo1.write(newval4);
+    Serial.print("4- ");
+    Serial.println(newval4);
+    oldval4=newval4;
 
-  potentiometerVal = analogRead(potentiometerPin);
-
- 
-
- //I use the map function because PWM pins can only accept
-
- //values from 0 to 255. Analog pins can output values from
-
- //0 to 1023. With the map function, the range 0-1023 is 
-
- //converted to appropriate values from 0 to 255.
-
- int mappedVal = map(potentiometerVal,0,1023,0,255);
-
- Serial.print(potentiometerVal);
-
- Serial.print(" - ");
-
- Serial.println(mappedVal);
-
- analogWrite(ledPin,mappedVal);
-
- delay(10); 
-
-}
-
-
-
+  } 
+  }
 
--- servo_pot/servo_pot.ino
+++ servo_pot/servo_pot.ino
@@ -1,25 +1,61 @@
+
 #include <Servo.h>
-Servo myservo;
+
+Servo myservo1;
+Servo myservo2;
+Servo myservo3;
+Servo myservo4;
+
+
 int potpin = 0;
-int val;
+int potpin2 = 1;
+int potpin3 = 2;
+int potpin4 = 3;
+
+int val = 0;
+int val2 = 0;
+int val3 = 0;
+int val4 = 0;
+
 void setup()
 {
-myservo.attach(9);
-Serial.begin(9600);
+
+myservo1.attach(5);
+myservo2.attach(9);
+myservo3.attach(10);
+myservo4.attach(11);
+
 }
+
 void loop()
 {
-val = analogRead(potpin);  
-         
-         
-val = map(val, 0, 1023, 0, 179); 
-         
-         
-myservo.write(val);    
-         
-delay(15);
 
-  Serial.println(val);
-  delay(1);        // delay in between reads for stability
-  
+val = analogRead(potpin);
+val = map(val, 3, 1023, 0, 176);
+
+myservo1.write(val);
+
+delay(25);
+
+val2 = analogRead(potpin2);
+val2 = map(val2, 3, 1023, 0, 176);
+
+myservo2.write(val2);
+
+delay(25);
+
+val3 = analogRead(potpin3);
+val3 = map(val3, 3, 1023, 0, 175);
+
+myservo3.write(val3);
+
+delay(25);
+
+val4 = analogRead(potpin4);
+val4 = map(val4, 3, 1023, 0, 175);
+
+myservo4.write(val3);
+
+delay(25);
+
 }
